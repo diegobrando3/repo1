e7cd3b4d022bf958380025d23527942cb603d17a
Enes <enes80330@gmail.com>
2026-05-21 19:34:49 +0300

    Delete Masaüstü directory

--- Masaüstü/kullanici_bilgi.py
+++
@@ -1,8 +0,0 @@
-def main():
-    isim=input("Adınız: ")
-    yas=int(input("Yaşınız: "))
-    sehir=input("Yaşadığınız şehir: ")
-    info=input("2 tane sevdiğiniz programlama dili / teknoloji")
-main() #fonksiyon çalıştırma
-print(f"Merhaba {isim}!,{yas} yaşındasın ve {sehir}'da yaşıyorsun. Şu anda {info} biliyorsun. 
-İleride umarım bu listeye 5 tane daha eklemiş olursun :)")
