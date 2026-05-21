240d233968444534b63d7af32192eb71518776fc
Enes <enes80330@gmail.com>
2026-05-21 19:35:22 +0300

    Add files via upload

---
+++ kullanici_bilgi.py
@@ -0,0 +1,8 @@
+def main():
+    isim=input("Adınız: ")
+    yas=int(input("Yaşınız: "))
+    sehir=input("Yaşadığınız şehir: ")
+    info=input("2 tane sevdiğiniz programlama dili / teknoloji")
+main() #fonksiyon çalıştırma
+print(f"Merhaba {isim}!,{yas} yaşındasın ve {sehir}'da yaşıyorsun. Şu anda {info} biliyorsun. 
+İleride umarım bu listeye 5 tane daha eklemiş olursun :)")
